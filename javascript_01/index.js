e71ebc16f0dcce3b7ec134f98b564654f940012f
¨Hugo Bravo <hubravo@gmail.com>
2019-09-18 23:25:43 -0300

    practica-js

---
+++ javascript_01/index.js
@@ -0,0 +1,28 @@
+var alumnos = [{
+    name: 'Martin',
+    lastname:'Barros',
+    age: '30',
+    country: 'Tuculandia',
+    hobbies: 'Footbal',
+    },
+ {  name: 'Fiorella',
+    lastname:'Di Porronni',
+    age: '53',
+    country: 'Peresolandia',
+    hobbies:'DORMIRRRRRRRRRRRRRRR',
+},
+ {
+    name: 'Matias',
+    lastname:'Aybar',
+    age: '35',
+    country: 'Argentina',
+    hobbies:'Beer',
+ }
+ {  name: 'Luis',
+    lastname:'Vasquez',
+    age: '29',
+    country: 'Argentina',
+    hobies:'Hablar',
+}];
+
+console.log(alumnos);
